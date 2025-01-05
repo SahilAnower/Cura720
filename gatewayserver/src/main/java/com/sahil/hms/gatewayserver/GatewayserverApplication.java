aa3565efee00ca0a8d87d42a31511f5eeddd8c1c
SahilAnower <sahilanower2@gmail.com>
2025-01-05 12:50:47 +0530

    Added working setup for gateway and eureka server

--- gatewayserver/src/main/java/com/sahil/hms/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/sahil/hms/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,28 @@ package com.sahil.hms.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes (RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(r -> r
+						.path("/curahms/patients/**")
+						.filters(f -> f.rewritePath("/curahms/patients/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://PATIENTS"))
+				.build();
 	}
 
 }
